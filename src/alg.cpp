c41b1319dcaa9e7e416a6423084fa73f21cee587
JuliaKorshunova <116427627+JuliaKorshunova@users.noreply.github.com>
2023-02-25 00:08:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a == 0 || b == 0)
-    {    
+    if (a == 0 || b == 0) {    
         return a+b;
-    }    
-    if (a > b)
-    {    
+    }
+    if (a > b){    
         return gcd(a-b, b);
-    }    
-    else
-    {   
+    } else {   
         return gcd(a, b-a);
-    }    
+    }
 }
